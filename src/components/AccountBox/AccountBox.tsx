3d76832d37ca9748a36a700de41d49716cd364fd
Erik Nucibella <eriknucib@live.it>
2023-09-23 11:05:42 -0400

    refactor(AccountBox): unlock next image patterns, render social profile and balance in AccountBox

--- src/components/AccountBox/AccountBox.tsx
+++ src/components/AccountBox/AccountBox.tsx
@@ -1,34 +1,42 @@
 import { FaEthereum } from "react-icons/fa";
 import { FormattingUtils } from "@/utils";
+import Image from "next/image";
+import { DEFUALT_USER_IMG_PLACEHOLDER } from "@/constants";
+import { useAccountInfo } from "@/hooks";
+import { ethers } from "ethers";
+import { useMemo } from "react";
 
 type Props = {
-  userImg: string;
   userAddress: string;
 };
 
 export const AccountBox = ({ userAddress }: Props) => {
+  const { ethBalance, userInfo } = useAccountInfo(userAddress);
+
+  const userEthBalance = useMemo(() => {
+    return FormattingUtils.formatNumberPrecision(
+      ethers.utils.formatEther(ethBalance ?? 0),
+      2
+    );
+  }, [ethBalance]);
+
   return (
     <div className="bg-[#FF89A9] rounded-md">
       <div className="px-2 py-1 flex flex-row items-center">
-        {/* <Image
-          width={8}
-          height={8}
+        <Image
+          width={32}
+          height={32}
           className="rounded-full"
-          src={
-            userImg ?? DEFUALT_USER_IMG_PLACEHOLDER //TODO: Default placeholder user image
-          }
+          src={userInfo?.profileImage ?? DEFUALT_USER_IMG_PLACEHOLDER}
           alt=""
-        /> */}
-        <div className="pl-2 text-white font-medium truncate">
-          {FormattingUtils.humanAddress(userAddress)}
-        </div>
+        />
         <div className="ml-2 bg-[#99324E] rounded-md">
           <div className="py-1 px-2 flex flex-row items-center">
             <div className="text-white">
               <FaEthereum />
             </div>
             <div className="pl-1 text-white font-medium ">
-              {0.0} ETH
+              {userEthBalance} ETH
               {/* TODO: Display real balance */}
             </div>
           </div>
