787f44c2e75502187a4776545623cc458f788bbb
Piero Bassa <pierobassa222@gmail.com>
2023-09-24 03:28:05 -0400

    refactor: account box

--- src/components/AccountBox/AccountBox.tsx
+++ src/components/AccountBox/AccountBox.tsx
@@ -1,47 +1,47 @@
-import { FaEthereum } from "react-icons/fa";
-import { FormattingUtils } from "@/utils";
-import Image from "next/image";
-import { DEFUALT_USER_IMG_PLACEHOLDER } from "@/constants";
-import { useAccountInfo } from "@/hooks";
-import { ethers } from "ethers";
-import { useMemo } from "react";
+import { ethers } from "ethers"
+import { useMemo } from "react"
+import { useAccountInfo } from "@/Hooks"
+import { FaEthereum } from "react-icons/fa"
+import Link from "next/link"
+import { DEFUALT_USER_IMG_PLACEHOLDER } from "../../Constants/Media"
+import { FormattingUtils } from "@/Utils"
 
 type Props = {
-  userAddress: string;
-};
+    userImg: string
+    userAddress: string
+}
 
-export const AccountBox = ({ userAddress }: Props) => {
-  const { ethBalance, userInfo } = useAccountInfo(userAddress);
+export const AccountBox = ({ userImg, userAddress }: Props) => {
+    const { ethBalance } = useAccountInfo(userAddress)
 
-  const userEthBalance = useMemo(() => {
-    return FormattingUtils.formatNumberPrecision(
-      ethers.utils.formatEther(ethBalance ?? 0),
-      2
-    );
-  }, [ethBalance]);
+    const userEthBalance = useMemo(() => {
+        return FormattingUtils.formatNumberPrecision(
+            ethers.utils.formatEther(ethBalance ?? 0),
+            2,
+        )
+    }, [ethBalance])
 
-  return (
-    <div className="bg-[#FF89A9] rounded-md">
-      <div className="px-2 py-1 flex flex-row items-center">
-        <Image
-          width={32}
-          height={32}
-          className="rounded-full"
-          src={userInfo?.profileImage ?? DEFUALT_USER_IMG_PLACEHOLDER}
-          alt=""
-        />
-        <div className="ml-2 bg-[#99324E] rounded-md">
-          <div className="py-1 px-2 flex flex-row items-center">
-            <div className="text-white">
-              <FaEthereum />
+    return (
+        <div className="bg-[#FF89A9] rounded-md">
+            <div className="px-2 py-1 flex flex-row items-center">
+                <img
+                    className="h-8 w-8 rounded-full"
+                    src={
+                        userImg ?? DEFUALT_USER_IMG_PLACEHOLDER //TODO: Default placeholder user image
+                    }
+                    alt=""
+                />
+                <div className="ml-2 bg-[#99324E] rounded-md">
+                    <div className="py-1 px-2 flex flex-row items-center">
+                        <div className="text-white">
+                            <FaEthereum />
+                        </div>
+                        <div className="pl-1 text-white font-medium ">
+                            {userEthBalance} ETH
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="pl-1 text-white font-medium ">
-              {userEthBalance} ETH
-              {/* TODO: Display real balance */}
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
