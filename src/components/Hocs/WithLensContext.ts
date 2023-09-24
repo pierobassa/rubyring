9d0d4d53f262f6e0fce33350548689c2496793e8
Piero Bassa <pierobassa222@gmail.com>
2023-09-24 03:28:51 -0400

    feat: hocs

---
+++ src/components/Hocs/WithLensContext.ts
@@ -0,0 +1,24 @@
+import { BiconomyLensContext, BiconomyLensProvider } from "@/Providers"
+import { useRouter } from "next/router"
+import { useContext } from "react"
+
+type Props = { Component: React.ReactNode }
+
+/**
+ *  This is a Higher Order Component that will render the component passed as a prop if the lensContext is available
+ * @param param0  Component is the component that will be rendered if the lensContext is available
+ * @returns
+ */
+export const WithLensContext: React.FC<Props> = ({ Component }) => {
+    const router = useRouter()
+
+    const lensContext = useContext(BiconomyLensContext)
+    console.log({ lensContext })
+
+    if (!lensContext) {
+        router.push("/")
+        return null
+    }
+
+    return Component
+}
