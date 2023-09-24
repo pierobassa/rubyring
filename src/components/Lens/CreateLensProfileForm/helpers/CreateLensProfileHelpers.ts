63102e821d4c9620e237b3fa95577e952aaa40e0
Erik Nucibella <eriknucib@live.it>
2023-09-24 07:36:54 -0400

    fix: web3.storage import

--- src/components/Lens/CreateLensProfileForm/helpers/CreateLensProfileHelpers.ts
+++ src/components/Lens/CreateLensProfileForm/helpers/CreateLensProfileHelpers.ts
@@ -1,4 +1,4 @@
-import { CIDString } from "web3.storage";
+import { CIDString } from "node_modules/web3.storage";
 
 export const getWeb3StorageLink = (CID: CIDString) => {
   return `https://${CID}.ipfs.w3s.link/`;
