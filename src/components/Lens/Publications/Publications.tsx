65d0dc03700b2a7c969f9bb4b86eb15c3a9bbd28
Erik Nucibella <eriknucib@live.it>
2023-09-24 07:05:48 -0400

    feat: render publications number

--- src/components/Lens/Publications/Publications.tsx
+++ src/components/Lens/Publications/Publications.tsx
@@ -1,6 +1,7 @@
 import { AnyPublication } from "@lens-protocol/react-web";
 import { LensPublication } from ".";
 import { useCallback } from "react";
+import { BaseSeparator } from "@/components";
 
 type Props = {
   publications: AnyPublication[];
@@ -29,23 +30,31 @@ export default function Publications({
 
   return (
     <div className="w-full flex flex-col md:flex-row gap-4 justify-between items-stretch flex-wrap">
-      {!publications.length && (
+      {!publications.length ? (
         <div className="w-full flex flex-col items-center justify-center">
           <div className="text-2xl font-bold">No posts yet</div>
           <div className="text-lg font-medium text-gray-500">
             {lensHandle} has not posted anything yet
           </div>
         </div>
-      )}
-      {publications.map((publication) => (
-        <div key={publication.id} className="w-full md:w-[30%]">
-          <LensPublication
-            content={getPublicationContent(publication)}
-            imageUrl={imageUrl}
-            lensHandle={lensHandle}
-          />
+      ) : (
+        <div className="w-full flex flex-col  justify-center gap-2">
+          <BaseSeparator className="w-full h-0.5 bg-stone-500" />
+          <div className="text-lg font-medium text-gray-500">
+            {publications.length} posts
+          </div>
+
+          {publications.map((publication) => (
+            <div key={publication.id} className="w-full md:w-[30%]">
+              <LensPublication
+                content={getPublicationContent(publication)}
+                imageUrl={imageUrl}
+                lensHandle={lensHandle}
+              />
+            </div>
+          ))}
         </div>
-      ))}
+      )}
     </div>
   );
 }
