1f5ffb4c88b94a461728cf7c9b341a41c315943a
Piero Bassa <pierobassa222@gmail.com>
2023-09-24 03:27:57 -0400

    refactor: assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,5 @@
-import * as GemLoader from "./gemLoader.json";
-import * as GemLoader3D from "./gem_loader_3d.json";
-import * as SuccessAnimation from "./success_onboarding.json";
+import * as GemLoader from "./gemLoader.json"
+import * as GemLoader3D from "./gem_loader_3d.json"
+import * as SuccessAnimation from "./success_onboarding.json"
 
-export { GemLoader, GemLoader3D, SuccessAnimation };
+export { GemLoader, GemLoader3D, SuccessAnimation }
